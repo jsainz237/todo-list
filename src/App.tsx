27e4980c86495c3e5c71c675e71f143ee5f63ed4
jsainz237 <jsainz237@gmail.com>
2021-07-22 20:20:43 -0500

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, { ThemeProvider } from 'styled-components';
+import BootstrapContainer from 'react-bootstrap/Container';
+import { theme } from './theme';
+import './App.scss';
+
+const AppWrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: ${props => props.theme.bg};
+`;
+
+const Container = styled(BootstrapContainer)`
+  justify-content: center;
+  align-items: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <AppWrapper>
+        <Container>
+          <div>HELLO</div>
+        </Container>
+      </AppWrapper>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,6 @@
+export const theme = {
+    bg: 'white',
+    fg: 'black',
+}
+
+export type ITheme = typeof theme;
